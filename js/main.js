e6df40d24093b4d5982615c1335009997d31ceb9
princenehanda <prince@grippx.co>
2025-07-26 21:37:08 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const hamb = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-links');
+hamb.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
 
+function toggleNav(){
+  document.querySelector('.nav-links').classList.toggle('active');
+}
