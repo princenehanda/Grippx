37b6a08a5af0f24e7cd02df2d1dad3caacda69c1
princenehanda <prince@grippx.co>
2025-07-27 04:31:46 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const hamb = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-links');
-hamb.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
-
-function toggleNav(){
-  document.querySelector('.nav-links').classList.toggle('active');
-}
